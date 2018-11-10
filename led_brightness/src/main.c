2a6236f90ac1de7dea15a464cf0da1d989855175
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-10 16:45:36 +0300

    led_brightness: simple led blinking with HAL

--- led_brightness/src/main.c
+++ led_brightness/src/main.c
@@ -11,10 +11,25 @@
 
 #include "stm32f3xx.h"
 #include "stm32f3xx_nucleo_144.h"
-			
+
+void init_led(void)
+{
+	GPIO_InitTypeDef init = {0};
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+	init.Pin = GPIO_PIN_13;
+	init.Mode = GPIO_MODE_OUTPUT_PP;
+	init.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOC, &init);
+}
 
 int main(void)
 {
+	HAL_Init();
+	init_led();
 
-	for(;;);
+	for(;;) {
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+		HAL_Delay(200);
+	}
 }
