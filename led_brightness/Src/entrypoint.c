2f5dfa6ded4a9fd98312125bc5cfcf7031f97723
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-11 18:33:04 +0300

    led_brightness: add entrypoint

---
+++ led_brightness/Src/entrypoint.c
@@ -0,0 +1,17 @@
+#include "led_driver.h"
+
+#define LED_BRIGHTNESS_50 50
+
+void set_half_brightness(led_t *led)
+{
+	led_set_brightness(led, LED_BRIGHTNESS_50);
+}
+
+void main(void)
+{
+	led_list_t *leds = leds_new();
+	leds_foreach(leds, set_half_brightness);
+
+	for (;;) {
+	}
+}
