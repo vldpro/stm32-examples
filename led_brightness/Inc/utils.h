6d0c04d8766cb755e99c2ecb2854a4be381bf622
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-12 23:30:19 +0300

    fixup! led_brightness: add utils

--- led_brightness/Inc/utils.h
+++ led_brightness/Inc/utils.h
@@ -1,7 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_h
 
+struct leds_list;
+
 void delay(int i);
-void test_leds_formation(leds_list_t *leds);
+void test_leds_formation(struct leds_list *leds);
 
 #endif
