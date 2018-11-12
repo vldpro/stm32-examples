b8be9f727adcdb511c6ebbbbf7cae2b23246fc95
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-12 23:30:38 +0300

    led_brightness: add animation

--- led_brightness/Src/entrypoint.c
+++ led_brightness/Src/entrypoint.c
@@ -1,17 +1,44 @@
 #include "led_driver.h"
+#include "utils.h"
 
-#define LED_BRIGHTNESS_50 50
+#define LED_BRIGHTNESS_DELTA 19
 
-void set_half_brightness(led_t *led)
+static void set_initial_brightness_gradient(leds_list_t *leds)
 {
-	led_set_brightness(led, LED_BRIGHTNESS_50);
+	unsigned int size = leds_size(leds);
+	led_set_brightness(leds_at(leds, 0), LED_MIN_BRIGHTNESS);
+	led_set_brightness(leds_at(leds, 1), LED_MIN_BRIGHTNESS);
+	led_set_brightness(leds_at(leds, 2), LED_MIN_BRIGHTNESS);
+	led_set_brightness(leds_at(leds, 3), 10);
+	led_set_brightness(leds_at(leds, 4), 30);
+	led_set_brightness(leds_at(leds, 5), 50);
+	led_set_brightness(leds_at(leds, 6), 70);
+	led_set_brightness(leds_at(leds, 7), 100);
+}
+
+static void brightness_shift(leds_list_t *leds)
+{
+	unsigned int size = leds_size(leds);
+
+	led_t *last_ld = leds_at(leds, size - 1);
+	unsigned int prev_br = led_get_brightness(last_ld);
+
+	for (unsigned int i = 0; i < size; i++) {
+		led_t *cur_led = leds_at(leds, i);
+
+		unsigned int tmp = led_get_brightness(cur_led);
+		led_set_brightness(cur_led, prev_br);
+		prev_br = tmp;
+		delay(100);
+	}
 }
 
 void main(void)
 {
 	leds_list_t *leds = leds_new();
-	leds_foreach(leds, set_half_brightness);
+	set_initial_brightness_gradient(leds);
 
 	for (;;) {
+		brightness_shift(leds);
 	}
 }
