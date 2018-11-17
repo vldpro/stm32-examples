3a59c21da6d8edd54ed0cabb2378eeca0f100a53
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-17 19:23:24 +0300

    led_brightness: utils: add bit_at()

--- led_brightness/Inc/utils.h
+++ led_brightness/Inc/utils.h
@@ -6,4 +6,9 @@ struct leds_list;
 void delay(int i);
 void test_leds_formation(struct leds_list *leds);
 
+inline int bit_at(unsigned char bitset, unsigned int i)
+{
+	return (bitset >> i) & 0x01;
+}
+
 #endif
