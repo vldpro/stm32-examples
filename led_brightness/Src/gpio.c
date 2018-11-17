fa93730ba9381c024937dfed85afbb55e21907b0
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-17 19:22:07 +0300

    led_brightness: regenerate project
    - remove TIM3
    - add PA10 in Input mode

--- led_brightness/Src/gpio.c
+++ led_brightness/Src/gpio.c
@@ -114,8 +114,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PA10 */
   GPIO_InitStruct.Pin = GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
