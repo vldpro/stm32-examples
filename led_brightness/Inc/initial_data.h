cb56e9cfe2e9479667cb00372aa0c665680557f7
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-18 19:29:49 +0300

    led_brightness: add init_data for led_driver

--- led_brightness/Inc/initial_data.h
+++ led_brightness/Inc/initial_data.h
@@ -1,16 +1,25 @@
 #ifndef INITIAL_DATA_H
 #define INITIAL_DATA_H
 
-#include "stm32f3xx_hal.h"
+#include <stdint.h>
+
+// btns_driver initial
 
 typedef struct btn_initial {
     uint16_t pin;
-    GPIO_TypeDef *port;
+    void *port;
 } btn_initial_t;
 
 typedef struct btns_initial {
     btn_initial_t *btns_inits;
     unsigned int sz;
 } btns_initial_t;
+
+// led_driver initial
+
+typedef struct leds_initial {
+    void *tims;
+    uint32_t sz;
+} leds_initial_t;
 
 #endif
