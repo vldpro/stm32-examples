1d502a1f039e18c0b38b5d426817b789313fb0d8
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-12-01 14:51:34 +0300

    usart: enable interrupts on uart4

--- usart/Src/stm32f3xx_it.c
+++ usart/Src/stm32f3xx_it.c
@@ -40,6 +40,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern UART_HandleTypeDef huart4;
 extern PCD_HandleTypeDef hpcd_USB_FS;
 
 /******************************************************************************/
@@ -204,6 +205,20 @@ void USB_LP_CAN_RX0_IRQHandler(void)
   /* USER CODE BEGIN USB_LP_CAN_RX0_IRQn 1 */
 
   /* USER CODE END USB_LP_CAN_RX0_IRQn 1 */
+}
+
+/**
+* @brief This function handles UART4 global interrupt / UART4 wake-up interrupt through EXTI line 34.
+*/
+void UART4_IRQHandler(void)
+{
+  /* USER CODE BEGIN UART4_IRQn 0 */
+
+  /* USER CODE END UART4_IRQn 0 */
+  HAL_UART_IRQHandler(&huart4);
+  /* USER CODE BEGIN UART4_IRQn 1 */
+
+  /* USER CODE END UART4_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
