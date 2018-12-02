5b7c5c7fc63006c85d2b7ff16f3bf0bfcc6498e0
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-12-02 17:46:53 +0300

    usart: add params to uart_init

--- usart/Inc/uart_int_driver.h
+++ usart/Inc/uart_int_driver.h
@@ -2,6 +2,7 @@
 #define UART_INT_DRIVER_H
 
 #include <stdint.h>
+#include "usart.h"
 
 typedef enum iuart_io_res {
     IUART_SUCCESS,
@@ -9,7 +10,11 @@ typedef enum iuart_io_res {
     IUART_BUF_FULL
 } iuart_res_t;
 
-void iuart_init(void);
+void iuart_init(UART_HandleTypeDef *uartHandle,
+                uint8_t *rbuf,
+                uint32_t rbuf_sz,
+                uint8_t *wbuf,
+                uint32_t wbuf_sz);
 
 iuart_res_t iuart_transmit(uint8_t val);
 iuart_res_t iuart_receive(uint8_t *val);
--- usart/Src/uart_int_driver.c
+++ usart/Src/uart_int_driver.c
@@ -11,38 +11,32 @@
 #include "fifo.h"
 
 //
-// Temporary globals
-//
-
-#define BUF_SZ 1024
-static uint8_t g_rbuf[BUF_SZ] = { 0 };
-static uint8_t g_wbuf[BUF_SZ] = { 0 };
-
-//
 // Module's internal structs
 //
 
 static struct mod_scope {
     fifo_t wfifo;
     fifo_t rfifo;
+    UART_HandleTypeDef *huart;
 };
 
 struct mod_scope mod_scope = { .wfifo = FIFO_INITIAL_DATA,
                                .rfifo = FIFO_INITIAL_DATA };
 
 //
-// Private functions
-//
-
-//
 // Public functions
 //
 
-void iuart_init(void)
+void iuart_init(UART_HandleTypeDef *uartHandle,
+                uint8_t *rbuf,
+                uint32_t rbuf_sz,
+                uint8_t *wbuf,
+                uint32_t wbuf_sz)
 {
-    fifo_init(&mod_scope.rfifo, g_rbuf, BUF_SZ);
-    fifo_init(&mod_scope.wfifo, g_wbuf, BUF_SZ);
-    __HAL_UART_ENABLE_IT(&huart4, UART_IT_RXNE);
+    fifo_init(&mod_scope.rfifo, rbuf, rbuf_sz);
+    fifo_init(&mod_scope.wfifo, wbuf, wbuf_sz);
+    mod_scope.huart = uartHandle;
+    __HAL_UART_ENABLE_IT(mod_scope.huart, UART_IT_RXNE);
 }
 
 iuart_res_t iuart_transmit(uint8_t val)
@@ -50,7 +44,7 @@ iuart_res_t iuart_transmit(uint8_t val)
     fifo_res_t res = fifo_push_back(&mod_scope.wfifo, val);
     if (res == FIFO_FULL)
         return IUART_BUF_FULL;
-    __HAL_UART_ENABLE_IT(&huart4, UART_IT_TXE);
+    __HAL_UART_ENABLE_IT(mod_scope.huart, UART_IT_TXE);
     return IUART_SUCCESS;
 }
 
